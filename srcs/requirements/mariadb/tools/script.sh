ce0ec4c00ed3142d0b851187d5fbc7f8c589eca0
esalmela <esalmela@esalmela.42.fr>
2025-03-06 11:49:20 +0200

    fixing mariadb script

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Debug: Print environment variables
-# echo "MYSQL_ROOT_PASSWORD: $MYSQL_ROOT_PASSWORD"
-# echo "WP_DB_NAME: $WP_DB_NAME"
-# echo "WP_DB_USER: $WP_DB_USER"
-# echo "WP_DB_PWD: $WP_DB_PWD"
-
 # Prepare directories and rights
 mkdir -p /var/lib/mysql /run/mysqld /var/log/mysql
 chown -R mysql:mysql /run/mysqld
@@ -13,10 +7,10 @@ chown -R mysql:mysql /var/log/mysql
 chown -R mysql:mysql /var/lib/mysql
 
 # init database
-mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
+mysqld --initialize --user=mysql --datadir=/var/lib/mysql
 
 # Enforce root pw, create db, add user, give rights
-mysqld --user=mysql --bootstrap << EOF
+mysqld --user=mysql --bootstrap << lim
 USE mysql;
 FLUSH PRIVILEGES;
 
@@ -25,9 +19,8 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 
-CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSQORD';
+CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
-# ALTER USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 
 # Check if the database exists before creating it
 CREATE DATABASE IF NOT EXISTS $WP_DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
@@ -39,6 +32,6 @@ GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%';
 GRANT ALL PRIVILEGES ON *.* TO '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PWD' WITH GRANT OPTION;
 GRANT SELECT ON mysql.* TO '$WP_DB_USER'@'%';
 FLUSH PRIVILEGES;
-EOF
+lim
 
 exec mysqld --defaults-file=/etc/my.cnf.d/mariadb-server.cnf
